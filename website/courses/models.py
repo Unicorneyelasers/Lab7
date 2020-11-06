1dd61fe4d020a4b1f0e5125f2f3a1da29ad33343
JMinerICS226 <ICS19-017@camosun.bc.ca>
2020-11-06 18:27:28 +0000

    Have coruse object

--- website/courses/models.py
+++ website/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    tag = models.CharField(max_length=1)
+    row = models.IntegerField()
+    col = models.IntegerField()
+
+    def _str_(self):
+        return self.tag + ' @(' + str(self.row) + ',' + str(self.col) + ')'
