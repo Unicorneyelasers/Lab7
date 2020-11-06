c4333618ba3674a836139f32df3c9faff2af9f20
JMinerICS226 <ICS19-017@camosun.bc.ca>
2020-11-06 18:56:52 +0000

    Part 4 works, but course named 1 / T FIX ME

--- website/courses/views.py
+++ website/courses/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from courses.models import Course
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello World!")
+    return HttpResponse("Hello World!")
+
+def get_course(request, id):
+    course = Course.objects.filter(id=id)
+    if(len(course) == 1):
+        return HttpResponse("Course %(id)s is at row %(row)s and col %(col)s" %{'id':course[0].tag, 'row':str(course[0].row), 'col':str(course[0].col)})
+    else:
+        return HttpsResponse("No such Course") 
